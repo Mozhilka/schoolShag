10656600fe714438aa6d55d8d5502f7273eb7b20
Mozhilka <87995228+Mozhilka@users.noreply.github.com>
2021-08-26 12:30:09 +0300

    First commit

--- src/main/java/school/lesson2/lesson3.java
+++ src/main/java/school/lesson2/lesson3.java
@@ -1,9 +1,14 @@
 package school.lesson2;
 
+
 public class lesson3 {
     public static void main(String[] args) {
-        for (int i=1; i<=10; i++) {
-            System.out.println ("5*" + i + "=" + 5*i);
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("2x" + i + " =  " + (2 * i) + "\t" + "3x" + i + " =  " + (3 * i)
+                    + "\t" + "4x" + i + " =  " + (4 * i)+ "\t" + "5x" + i + " =  " + (5 * i)
+                    + "\t" + "6x" + i + " =  " + (6 * i)+ "\t" + "7x" + i + " =  " + (7 * i)
+                    + "\t" + "8x" + i + " =  " + (8 * i)+ "\t" + "9x" + i + " =  " + (9 * i)
+                    + "\t" + "10x" + i + " =  " + (10 * i));
         }
     }
 }
